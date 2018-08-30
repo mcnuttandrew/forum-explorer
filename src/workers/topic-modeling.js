7229e505e8c5bf7bdba461f2fbf2ddd152e5577f
Andrew McNutt <mcnutt.andrew@gmail.com>
2018-08-30 16:50:16 -0500

    i guess were going down the server road

--- src/workers/topic-modeling.js
+++ src/workers/topic-modeling.js
@@ -1,16 +1,27 @@
 // import lda from 'lda';
-const lda = require('lda');
+// const lda = require('lda');
+const lda = require('../lda-fork/lda');
 /* eslint-disable consistent-this */
 module.exports = function worker(self) {
   const sendMessage = msg => self.postMessage(msg);
   self.addEventListener('message', event => {
-    // console.log(event);
-    console.log(event.data)
+    const strings = event.data.map(row => row.trim().replace(/reply$/, '').trim());
+    console.log('preparing model')
     new Promise((resolve, reject) => resolve())
-    .then(() => lda(event.data, 2, 5))
+    .then(() => lda(strings, 5, 1, ['en'], null, null, 10))
     .then(model => {
-      console.log(model)
-      sendMessage(model);
+      const dedupedModel = model.reduce((acc, row) => {
+        console.log(acc, row)
+        const modelTerm = row[0];
+        if (!acc[modelTerm.term]) {
+          acc[modelTerm.term] = 0;
+        }
+        acc[modelTerm.term] += modelTerm.probability;
+        return acc;
+      }, {});
+      console.log(model, dedupedModel, Object.entries(dedupedModel))
+
+      sendMessage(Object.entries(dedupedModel));
     });
   });
 };
