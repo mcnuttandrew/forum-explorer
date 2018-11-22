77363a4029728cc5a969ef3baff1b5d19a245946
Andrew McNutt <mcnutt.andrew@gmail.com>
2018-11-21 23:49:39 -0600

    update build pipeline wrt logo

--- prepare-build.sh
+++ prepare-build.sh
@@ -2,9 +2,7 @@ yarn
 mv src/constants/test-data.json src/constants/test-data-large.json
 mv src/constants/small-data.json src/constants/test-data.json
 yarn build
-rm -rf dist
-mkdir dist
+rm dist/bundle.js
 cp bundle.js ./dist/
-cp manifest.json ./dist/
 mv src/constants/test-data.json src/constants/small-data.json
 mv src/constants/test-data-large.json src/constants/test-data.json
