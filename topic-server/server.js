253b1f21171f3f3b893933f7512a9b2fd46b992f
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2019-08-03 14:06:34 -0700

    First bit of the mongo stuff

--- topic-server/server.js
+++ topic-server/server.js
@@ -1,14 +1,23 @@
 /* eslint-disable no-console */
-const lda = require('lda');
+const MongoClient = require('mongodb').MongoClient;
+const {
+  fetchAllModels,
+  fetchAllVisits,
+  log,
+  recordModel,
+  recordVisit,
+  fetchModel,
+  stripAndModel,
+  sleep
+} = require('./utils');
 const express = require('express');
 const app = express();
 const request = require('request');
-const cheerio = require('cheerio');
 /* eslint-disable */
 const PORT = process.env.PORT || 5000;
+const MONGO_URL = 'mongodb://localhost:27017';
+const MONGO_DB = 'FEX_DB';
 /* eslint-enable */
-const sleep = delay => new Promise((resolve, reject) => setTimeout(d => resolve(), delay));
-const log = msg => console.log(`${new Date().getTime()}: ${msg}`);
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -16,49 +25,56 @@ app.use((req, res, next) => {
   next();
 });
 
-const modelCache = {};
 const inProgress = {};
 
-function stripAndModel(html, topics, terms) {
-  const $ = cheerio.load(html);
-  const texts = $('.comment').text()
-    .replace(/reply/g, '')
-    .split('\n')
-    .map(d => d.trim())
-    .filter(d => d.length);
-
-  return lda(texts, topics, terms, ['en'], null, null, 10).filter(d => d.length);
-}
-
 function parseAndModel(req, res) {
+  const db = req.app.locals.db;
+
   const item = req.query && req.query.item;
   const topics = req.query && req.query.topics || 5;
   const terms = req.query && req.query.terms || 15;
   const cacheId = `${item}-${topics}-${terms}`;
+  const requestConfig = {item, topics, terms, cacheId};
   if (!item) {
     res.send('Invalid query');
     return;
   }
+  recordVisit(db, item);
   log(`request for ${item}`);
-  const currentTime = new Date().getTime();
-  const fiveMinutes = 5 * 60 * 1000;
-  const cacheItem = modelCache[cacheId];
-  if (cacheItem && ((cacheItem.time - currentTime) < fiveMinutes)) {
-    log(`request for ${item} fulfilled by cache`);
-    // TODO: record cache hit, if cache hits happen to many times invalidate the cache
-    res.send(modelCache[cacheId].model);
-    return;
-  }
 
-  res.status(204).send('NONE FOUND, BUILDING');
   if (inProgress[cacheId]) {
+    res.status(204).send('BUILDING');
     return;
   }
+  fetchModel(db, item)
+    .then(model => {
+      const currentTime = new Date().getTime();
+      const fiveMinutes = 5 * 60 * 1000;
+      if (model && model.length === 1 && (model[0].time - currentTime) < fiveMinutes) {
+        // TODO: record cache hit, if cache hits happen to many times invalidate the cache
+        log(`request for ${item} fulfilled by cache`);
+        res.send(model[0].model);
+        return;
+      }
+
+      res.status(204).send('NONE FOUND, BUILDING');
+      return buildModel(requestConfig, db);
+    })
+    .catch(error => {
+      inProgress[cacheId] = false;
+      console.log(error);
+      log(`error ${error}`);
+    });
+}
+
+function buildModel(requestConfig, db) {
+  const {item, topics, terms, cacheId} = requestConfig;
   inProgress[cacheId] = true;
   const startTime = new Date().getTime();
   const hnURL = `https://news.ycombinator.com/item?id=${item}`;
 
-  sleep(Math.floor(Math.random() * 2000)).then(
+  return sleep(Math.floor(Math.random() * 2000))
+  .then(
     () => new Promise((resolve, reject) => request(hnURL, (error, response, html) => {
       log(`recieved html for ${item}`);
       if (!error && response.statusCode === 200) {
@@ -80,20 +96,29 @@ function parseAndModel(req, res) {
     const model = stripAndModel(html, topics, terms);
     const endTime = new Date().getTime();
     log(`modeling for ${item} complete. took ${(endTime - startTime) / 1000} seconds. caching model`);
-    // res.send(model);
     inProgress[cacheId] = false;
-    modelCache[cacheId] = {
-      time: currentTime,
-      model
-    };
-  })
-  .catch(error => {
-    inProgress[cacheId] = false;
-    log(`error ${error}`);
+    recordModel(db, item, model);
   });
 }
 
+app.get('/analytics', (req, res) =>
+  Promise.all([
+    fetchAllModels(req.app.locals.db),
+    fetchAllVisits(req.app.locals.db)
+  ])
+  .then(([models, visits]) => {
+    res.send(JSON.stringify({models, visits}, null, 2));
+  }));
+
 app.get('/', (req, res) => parseAndModel(req, res));
 
-app.listen(PORT, () => log(`listening on ${PORT}`));
+MongoClient.connect(MONGO_URL, {}, (err, client) => {
+  if (err) {
+    log(`Failed to connect to the database. ${err.stack}`);
+  }
+  app.locals.db = client.db(MONGO_DB);
+  app.listen(PORT, () => {
+    log(`Node.js app is listening at http://localhost:${PORT}`);
+  });
+});
 /* eslint-enable no-console */
---
+++ topic-server/utils.js
@@ -0,0 +1,58 @@
+const lda = require('lda');
+const cheerio = require('cheerio');
+function stripAndModel(html, topics, terms) {
+  const $ = cheerio.load(html);
+  const texts = $('.comment').text()
+    .replace(/reply/g, '')
+    .split('\n')
+    .map(d => d.trim())
+    .filter(d => d.length);
+
+  return lda(texts, topics, terms, ['en'], null, null, 10).filter(d => d.length);
+}
+
+const recordVisit = (db, itemId) => new Promise((resolve, reject) => {
+  const cb = (err, result) => err ? reject(err) : resolve(result);
+  db.collection('visits')
+    .insertOne({itemId, time: new Date().getTime()}, cb);
+});
+
+const recordModel = (db, itemId, model) => new Promise((resolve, reject) => {
+  const cb = (err, result) => err ? reject(err) : resolve(result);
+  db.collection('models')
+    .updateOne({itemId}, {$set: {time: new Date().getTime(), model}}, {upsert: true}, cb);
+});
+
+const fetch = (db, collectionName, query, limit) => {
+  const collection = db.collection(collectionName);
+  return new Promise((resolve, reject) => {
+    const cursor = collection.find(query);
+    const cb = (err, result) => err ? reject(err) : resolve(result);
+    if (limit) {
+      cursor.limit(1).toArray(cb);
+      return;
+    }
+    cursor().toArray(cb);
+  });
+};
+const fetchModel = (db, itemId) => fetch(db, 'model', {itemId}, 1);
+const fetchAllModels = (db, itemId) => fetch(db, 'model', {}, false);
+const fetchVisitsByItem = (db, itemId) => fetch(db, 'visits', {itemId}, false);
+const fetchAllVisits = (db, itemId) => fetch(db, 'visits', {}, false);
+
+const sleep = delay => new Promise((resolve, reject) => setTimeout(d => resolve(), delay));
+/* eslint-disable */
+const log = msg => console.log(`${new Date().getTime()}: ${msg}`);
+/* eslint-enable */
+
+module.exports = {
+  stripAndModel,
+  recordModel,
+  fetchModel,
+  recordVisit,
+  fetchVisitsByItem,
+  fetchAllVisits,
+  fetchAllModels,
+  sleep,
+  log
+};
