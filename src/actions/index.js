a7b3ebdd8aa0f7855b7cae7c28b7505aa93929be
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2020-07-30 19:38:48 -0500

    rewrite server to deal mongo migration

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,17 +2,13 @@
 import {CHILD_THRESHOLD, SERVER_DEV_MODE} from '../constants';
 import {prepareTree, log} from '../utils';
 import {executeRequest} from '../api-calls';
-import {
-  checkForTour,
-  getTreeForId,
-  getSettingsFromDb
-} from './db';
+import {checkForTour, getTreeForId, getSettingsFromDb} from './db';
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 
-const buildEasyAction = type => payload => dispatch => dispatch({type, payload});
+const buildEasyAction = (type) => (payload) => (dispatch) => dispatch({type, payload});
 export const clearSelection = buildEasyAction('clear-selection');
 export const finishTour = buildEasyAction('finish-tour');
 export const lockAndSearch = buildEasyAction('lock-and-search');
@@ -20,10 +16,10 @@ export const setFoundOrder = buildEasyAction('set-found-order');
 export const setHoveredComment = buildEasyAction('set-hovered-comment');
 export const setSearch = buildEasyAction('set-search');
 export const setSelectedCommentPath = buildEasyAction('set-comment-path');
-export const setSelectedCommentPathWithGraphComment =
-  buildEasyAction('set-comment-path-with-graph-comment');
-export const setSelectedCommentPathWithSelectionClear =
-  buildEasyAction('set-comment-path-with-selection-clear');
+export const setSelectedCommentPathWithGraphComment = buildEasyAction('set-comment-path-with-graph-comment');
+export const setSelectedCommentPathWithSelectionClear = buildEasyAction(
+  'set-comment-path-with-selection-clear',
+);
 export const setShowTour = buildEasyAction('show-tour');
 export const unsetGraphComment = buildEasyAction('unset-graph-comment');
 export const setTimeFilter = buildEasyAction('set-time-filter');
@@ -32,99 +28,103 @@ export const unlockAndSearch = buildEasyAction('unlock-and-search');
 export const updateGraphPanelDimensions = buildEasyAction('update-graph-panel-dimensions');
 
 const EXTENSION_MODE = window.origin === 'https://news.ycombinator.com';
-const dispatchRequest = EXTENSION_MODE ?
-  details => new Promise((resolve, reject) => chrome.runtime.sendMessage(details, resolve)) :
-  details => executeRequest(details);
+const dispatchRequest = EXTENSION_MODE
+  ? (details) => new Promise((resolve, reject) => chrome.runtime.sendMessage(details, resolve))
+  : (details) => executeRequest(details);
 
-const cleanModels = models => models.map(row => row.map(d => ({...d, term: d.term.split('\'')[0]})));
+const cleanModels = (models) => models.map((row) => row.map((d) => ({...d, term: d.term.split("'")[0]})));
 
-export const sleep = sleepTime => x => new Promise(resolve => setTimeout(() => resolve(x), sleepTime));
+export const sleep = (sleepTime) => (x) => new Promise((resolve) => setTimeout(() => resolve(x), sleepTime));
 
-export const checkIfTourShouldBeShown = () => dispatch => checkForTour()
-    .then(payload => dispatch({type: 'check-if-tour-should-be-shown', payload}));
-export const modelData = item => dispatch => {
+export const checkIfTourShouldBeShown = () => (dispatch) =>
+  checkForTour().then((payload) => dispatch({type: 'check-if-tour-should-be-shown', payload}));
+export const modelData = (item) => (dispatch) => {
   dispatchRequest({
     template: SERVER_DEV_MODE ? 'modelFullPageTemplateDevMode' : 'modelFullPageTemplate',
-    item
+    item,
   })
     .then(cleanModels)
-    .then(payload => dispatch({type: 'model-data', payload}))
+    .then((payload) => dispatch({type: 'model-data', payload}))
     .catch(() => {});
 };
 
 export const modelBranches = (dispatch, data, root, tree) => {
-  const items = tree.children
-    .filter((d) => d.descendants >= CHILD_THRESHOLD)
-    .map(({id}) => id);
+  const items = tree.children.filter((d) => d.descendants >= CHILD_THRESHOLD).map(({id}) => id);
   log('modeling branches', items.length);
   let current = 0;
-  Promise.all(items.map(item => {
-    return dispatchRequest({
-      template: SERVER_DEV_MODE ? 'modelSingleBranchTemplateDevMode' : 'modelSingleBranchTemplate',
-      item
+  Promise.all(
+    items.map((item) => {
+      return dispatchRequest({
+        template: SERVER_DEV_MODE ? 'modelSingleBranchTemplateDevMode' : 'modelSingleBranchTemplate',
+        item,
+      })
+        .then(cleanModels)
+        .then((d) => {
+          current += 1;
+          log(`modeled ${current} / ${items.length}`);
+          return d;
+        })
+        .then((model) => ({item, model: (model[0] && model[0][0]) || null}));
+    }),
+  )
+    .then((payload) => {
+      return payload.reduce((acc, {item, model}) => {
+        acc[item] = model;
+        return acc;
+      }, {});
     })
-    .then(cleanModels)
-    .then(d => {
-      current += 1;
-      log(`modeled ${current} / ${items.length}`);
-      return d;
-    })
-    .then(model => ({item, model: model[0] && model[0][0] || null}));
-  }))
-  .then(payload => {
-    return payload.reduce((acc, {item, model}) => {
-      acc[item] = model;
-      return acc;
-    }, {});
-  })
-  .then(payload => dispatch({type: 'model-branches', payload}));
+    .then((payload) => dispatch({type: 'model-branches', payload}));
 };
 
-export const setConfig = (configCategory, configValue) => dispatch => dispatch({
-  type: 'set-config-value',
-  payload: {configCategory, configValue}
-});
+export const setConfig = (configCategory, configValue) => (dispatch) =>
+  dispatch({
+    type: 'set-config-value',
+    payload: {configCategory, configValue},
+  });
 
 // not currently in use
 export function getAllUsers(dispatch, data) {
-  const users = Object.keys(data.reduce((acc, row) => {
-    acc[row.by] = true;
-    return acc;
-  }, {}));
-  Promise.all(users.map(item => dispatchRequest({template: 'userTemplate', item})))
-    .then(userData => dispatch({
+  const users = Object.keys(
+    data.reduce((acc, row) => {
+      acc[row.by] = true;
+      return acc;
+    }, {}),
+  );
+  Promise.all(users.map((item) => dispatchRequest({template: 'userTemplate', item}))).then((userData) =>
+    dispatch({
       type: 'get-all-users',
-      payload: userData
-    }));
+      payload: userData,
+    }),
+  );
 }
 
-export const getAllItems = (root, ignoreSettingsUpdate) => dispatch => {
+export const getAllItems = (root, ignoreSettingsUpdate) => (dispatch) => {
   let children = [];
   let depth = 0;
-  const doGeneration = generation =>
-    Promise.all(generation.map(item => dispatchRequest({template: 'hnTemplate', item})))
-    .then(offspring => {
-      dispatch({
-        type: 'increase-loaded-count',
-        payload: {newCount: children.length}
-      });
-      children = children.concat(offspring.map(d => ({children: [], ...d, depth})));
-      depth += 1;
-      const newgen = offspring
-        .reduce((acc, child) => acc.concat(child && child.kids || []), []);
-      if (newgen.length) {
-        return doGeneration(newgen);
-      }
-      return children;
-    });
+  const doGeneration = (generation) =>
+    Promise.all(generation.map((item) => dispatchRequest({template: 'hnTemplate', item}))).then(
+      (offspring) => {
+        dispatch({
+          type: 'increase-loaded-count',
+          payload: {newCount: children.length},
+        });
+        children = children.concat(offspring.map((d) => ({children: [], ...d, depth})));
+        depth += 1;
+        const newgen = offspring.reduce((acc, child) => acc.concat((child && child.kids) || []), []);
+        if (newgen.length) {
+          return doGeneration(newgen);
+        }
+        return children;
+      },
+    );
 
   return Promise.resolve()
     .then(() => doGeneration([root]))
-    .then(data => {
+    .then((data) => {
       const tree = prepareTree(data, root);
       dispatch({
         type: 'get-all-items',
-        payload: {data, root, tree, ignoreSettingsUpdate}
+        payload: {data, root, tree, ignoreSettingsUpdate},
       });
       // construct models for the relevant branches
       modelBranches(dispatch, data, root, tree);
@@ -134,47 +134,47 @@ export const getAllItems = (root, ignoreSettingsUpdate) => dispatch => {
       // maybe call getAllItems again if it's a new-ish thread
       const time = tree.data.data.time * 1000;
       const currentTime = new Date().getTime();
-      if ((currentTime - time) > DAY) {
+      if (currentTime - time > DAY) {
         return;
       }
       setTimeout(() => getAllItems(root, true)(dispatch), 40 * SECOND);
     });
 };
 
-export const setPageId = payload => dispatch => {
+export const setPageId = (payload) => (dispatch) => {
   dispatch({type: 'set-page-id', payload});
-  getTreeForId(payload)
-    .then(result => dispatch({
+  getTreeForId(payload).then((result) =>
+    dispatch({
       type: 'get-tree-from-cache',
       payload: {
         data: result,
-        pageId: payload
-      }
-    }));
+        pageId: payload,
+      },
+    }),
+  );
 };
 
-export const getItemsFromCacheOrRedirect = payload => dispatch => {
-  getTreeForId(payload)
-    .then(result => {
-      // redirect if not found
-      if (!result) {
-        window.location.href = `?id=${payload}`;
-        return;
-      }
-      // if found pull from cache & update the location url
-      window.history.pushState('', '', `?id=${payload}`);
-      dispatch({
-        type: 'get-tree-from-cache',
-        payload: {
-          data: result,
-          pageId: payload
-        }
-      });
+export const getItemsFromCacheOrRedirect = (payload) => (dispatch) => {
+  getTreeForId(payload).then((result) => {
+    // redirect if not found
+    if (!result) {
+      window.location.href = `?id=${payload}`;
+      return;
+    }
+    // if found pull from cache & update the location url
+    window.history.pushState('', '', `?id=${payload}`);
+    dispatch({
+      type: 'get-tree-from-cache',
+      payload: {
+        data: result,
+        pageId: payload,
+      },
     });
+  });
 };
 
-export const getSettingsFromCache = () => dispatch => getSettingsFromDb()
-  .then(payload => {
+export const getSettingsFromCache = () => (dispatch) =>
+  getSettingsFromDb().then((payload) => {
     if (!payload) {
       return;
     }
--- topic-server/utils.js
+++ topic-server/utils.js
@@ -1,69 +1,93 @@
 const lda = require('lda');
 const cheerio = require('cheerio');
+const {query} = require('./db');
+const fetch = require('node-fetch');
 
 const getTime = () => new Date().getTime();
-const sleep = delay => new Promise((resolve, reject) => setTimeout(d => resolve(), delay));
+const sleep = (delay) => new Promise((resolve, reject) => setTimeout((d) => resolve(), delay));
 /* eslint-disable */
-const log = msg => console.log(`${new Date().getTime()}: ${msg}`);
-/* eslint-enable */
-const hnTemplate = id => `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
-const cleanItem = item => ['by', 'id', 'parent', 'time', 'title'].reduce((acc, key) => {
-  acc[key] = item[key];
-  return acc;
-}, {});
+const log = (msg) => console.log(`${new Date().getTime()}: ${msg}`);
+// const log = () => {};
+const hnTemplate = (id) => `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
 
 function stripAndModel(html, topics, terms) {
   const $ = cheerio.load(html);
-  const texts = $('.comment').text()
+  const texts = $('.comment')
+    .text()
     .replace(/reply/g, '')
     .split('\n')
-    .map(d => d.trim())
-    .filter(d => d.length);
+    .map((d) => d.trim())
+    .filter((d) => d.length);
 
-  return lda(texts, topics, terms, ['en'], null, null, 10).filter(d => d.length);
+  return lda(texts, topics, terms, ['en'], null, null, 10).filter((d) => d.length);
 }
 
-const record = (db, collectionName, query, update) => new Promise((resolve, reject) => {
-  const cb = (err, result) => err ? reject(err) : resolve(result);
-  db.collection(collectionName)
-    .updateOne(query, update, {upsert: true}, cb);
-});
-const recordModel = (db, itemId, model) =>
-  record(db, 'models', {itemId}, {$set: {time: getTime(), model}});
-const recordVisit = (db, itemId) =>
-  record(db, 'visits', {itemId}, {$push: {time: getTime()}});
-const recordItemData = (db, item) =>
-  record(db, 'itemData', {itemId: item.id}, {$set: {time: getTime(), item: cleanItem(item)}});
+const fetchModel = (itemId, numTopics, numTerms) => {
+  const getModel = 'SELECT * FROM models WHERE item_id=$1 AND num_topics=$2 AND num_terms=$3;';
+  return query(getModel, [itemId, numTopics, numTerms]);
+};
 
-const fetch = (db, collectionName, query, limit) => {
-  const collection = db.collection(collectionName);
-  return new Promise((resolve, reject) => {
-    const cursor = collection.find(query);
-    const cb = (err, result) => err ? reject(err) : resolve(result);
-    if (limit) {
-      cursor.limit(1).toArray(cb);
-      return;
+const recordModel = (itemId, numTopics, numTerms, model) => {
+  const createModel = `
+INSERT INTO models 
+(item_id, num_topics, num_terms, model, visits, data) 
+VALUES ($1, $2, $3, $4, $5, $6);
+  `;
+  const updateModel = `
+UPDATE models
+SET model=$1
+WHERE item_id=$2 AND num_topics=$3 AND num_terms=$4;
+`;
+  const checkedModel = JSON.stringify(model);
+  const visits = JSON.stringify([getTime()]);
+  return fetchModel(itemId, numTopics, numTerms).then((result) => {
+    // if there's nothing there then create it
+    if (!result.rows.length) {
+      return fetch(hnTemplate(itemId))
+        .then((d) => d.json())
+        .then((data) => {
+          return query(createModel, [
+            itemId,
+            numTopics,
+            numTerms,
+            checkedModel,
+            visits,
+            JSON.stringify(data),
+          ]);
+        });
     }
-    cursor.toArray(cb);
+    // if there's something there, then update it
+    return query(updateModel, [checkedModel, itemId, numTopics, numTerms]);
   });
 };
-const fetchModel = (db, itemId) => fetch(db, 'models', {itemId}, 1);
-const fetchAllModels = (db, itemId) => fetch(db, 'models', {}, false);
-const fetchVisitsByItem = (db, itemId) => fetch(db, 'visits', {itemId}, false);
-const fetchAllVisits = (db, itemId) => fetch(db, 'visits', {}, false);
-const fetchItemData = (db, itemId) => fetch(db, 'itemData', {itemId}, 1);
+
+const recordVisit = (itemId, numTopics, numTerms) =>
+  fetchModel(itemId, numTopics, numTerms).then((result) => {
+    if (!result.rows.length) {
+      return recordModel(itemId, numTopics, numTerms, []);
+    }
+    // get visit from result
+    const updatedVisits = [...result.rows[0].visits, getTime()];
+    // from this get the result
+    query(
+      `
+      UPDATE models
+      SET visits=$1
+      WHERE item_id=$2 AND num_topics=$3 AND num_terms=$4;
+      `,
+      [JSON.stringify(updatedVisits), itemId, numTopics, numTerms],
+    );
+  });
+
+const fetchAll = () => query('SELECT * FROM models;');
 
 module.exports = {
-  fetchAllVisits,
-  fetchAllModels,
-  fetchItemData,
+  fetchAll,
   fetchModel,
-  fetchVisitsByItem,
+  recordModel,
+  recordVisit,
   hnTemplate,
   log,
   sleep,
   stripAndModel,
-  recordItemData,
-  recordModel,
-  recordVisit
 };
